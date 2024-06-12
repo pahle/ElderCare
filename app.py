1fde958ed922df1c5f33a03b878792d17fcd3dfe
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-06-13 00:03:48 +0700

    fix response

--- app.py
+++ app.py
@@ -3,28 +3,56 @@ import json
 from tensorflow.keras.models import load_model
 import numpy as np
 
+# Initialize the Flask application
 app = Flask(__name__)
-model = load_model('model.h5')
-sickness = json.load(open('sickness.json'))["data"]
 
+# Load the model
+model = load_model('model.h5')
+
+# Load the disease data
+disease = json.load(open('disease.json'))["data"]
+
+@app.route("/")
+def get_index():
+  return get_index_handler()
 
 @app.route("/predict", methods=["POST"])
-def predict():
-  input = request.get_json()
-  if input is None:
-    return jsonify({"error": "No data provided"}), 400
+def post_predict():
+  return post_predict_handler()
 
-  try:
-    data = np.array(input["data"]).reshape(1, -1)
-  except KeyError:
-    return jsonify({"error": "Missing key 'data' in request"}), 400
+def get_index_handler():
+  return jsonify({
+        "status": "success",
+        "code": 200,
+        "message": "Successfully connected to the API",
+    }), 200
   
-  
+def post_predict_handler():
+  request_data = request.json
+  print(request_data)
+    
+  if "data" not in request_data:
+    return jsonify({
+        "status": "fail",
+        "code": 400,
+        "message": "Data is missing in the request body",
+    }), 400
+
+  data = np.array(request_data["data"]).reshape(1, -1)
+    
   prediction = model.predict(data)
 
   predicted_label_index = np.argmax(prediction)
 
-  return jsonify({"prediction": str(sickness[predicted_label_index]), "accuracy": str(prediction[0][predicted_label_index])})
-
+  return jsonify({
+    "status": "success",
+    "code": 200,
+    "message": "Prediction successful",
+    "data": {
+      "prediction": disease[predicted_label_index],
+      "probability": str(int(prediction[0][predicted_label_index] * 100)) + "%"
+    }
+  }), 200
+  
 if __name__ == "__main__":
   app.run(debug=True)
