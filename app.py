a4cece4932f4ddb586f23bcf64acc40f150596d9
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-06-12 21:07:47 +0700

    finalize backend for prod

--- app.py
+++ app.py
@@ -1,87 +1,30 @@
 from flask import Flask, request, jsonify
+import json
 from tensorflow.keras.models import load_model
 import numpy as np
 
 app = Flask(__name__)
 model = load_model('model.h5')
+sickness = json.load(open('sickness.json'))["data"]
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
-  # Get data from request (assuming JSON)
-  data = request.get_json()
-  if data is None:
+  input = request.get_json()
+  if input is None:
     return jsonify({"error": "No data provided"}), 400
 
-  # Extract array from data (assuming key 'data')
   try:
-    array = data["data"]
+    data = np.array(input["data"]).reshape(1, -1)
   except KeyError:
     return jsonify({"error": "Missing key 'data' in request"}), 400
   
-  sickness= [
-        "Fungal infection",
-        "Allergy",
-        "GERD",
-        "Chronic cholestasis",
-        "Drug Reaction",
-        "Peptic ulcer disease",
-        "AIDS",
-        "Diabetes",
-        "Gastroenteritis",
-        "Bronchial Asthma",
-        "Hypertension",
-        "Migraine",
-        "Cervical spondylosis",
-        "Paralysis (brain hemorrhage)",
-        "Jaundice",
-        "Malaria",
-        "Chicken pox",
-        "Dengue",
-        "Typhoid",
-        "Hepatitis A",
-        "Hepatitis B",
-        "Hepatitis C",
-        "Hepatitis D",
-        "Hepatitis E",
-        "Alcoholic hepatitis",
-        "Tuberculosis",
-        "Common Cold",
-        "Pneumonia",
-        "Dimorphic hemorrhoids (piles)",
-        "Heart attack",
-        "Varicose veins",
-        "Hypothyroidism",
-        "Hyperthyroidism",
-        "Hypoglycemia",
-        "Osteoarthritis",
-        "Arthritis",
-        "Paroxysmal Positional Vertigo",
-        "Acne",
-        "Urinary tract infection",
-        "Psoriasis",
-        "Impetigo",
-        "Fungal infection"
-    ]
+  
+  prediction = model.predict(data)
 
-  sample_input = np.array(array)
+  predicted_label_index = np.argmax(prediction)
 
-  sample_input = sample_input.reshape(1, -1)
-
-    # Predict using your model_script (assuming predict function defined there)
-  prediction = model.predict(sample_input)  # Pass model and data
-
-  # find the highest value in the prediction array
-  highest = np.argmax(prediction[0])
-
-  second_highest = np.argsort(prediction[0])[-2]
-
-  # print(sickness[highest])
-  print(second_highest)
-  print(prediction[0][second_highest])
-
-  # Return prediction as JSON
-  # return jsonify({"prediction": prediction[0].tolist()})
-  return jsonify({"first_prediction": sickness[highest], "first_accuracy": str(prediction[0][highest]), "second_prediction": sickness[second_highest], "second_accuracy": str(prediction[0][second_highest])})
+  return jsonify({"prediction": str(sickness[predicted_label_index]), "accuracy": str(prediction[0][predicted_label_index])})
 
 if __name__ == "__main__":
   app.run(debug=True)
