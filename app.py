15212cb8b375d37cf12ff9d1f319151187b0ff60
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-06-13 11:21:10 +0700

    fix model backend for deployment

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ app = Flask(__name__)
 # Load the model
 model = load_model('model.h5')
 
-# Load the disease data
-disease = json.load(open('disease.json'))["data"]
+# Load the data
+list_diseases = json.load(open('list_diseases.json'))
+db_diseases = json.load(open('db_diseases.json'))
+symptoms = json.load(open('list_symptoms.json'))
+
 
 @app.route("/")
 def get_index_handler():
@@ -23,35 +26,44 @@ def get_index_handler():
 
 @app.route("/predict", methods=["POST"])
 def post_predict_handler():
-  request_data = request.json
-  print(request_data)
+  data = request.json
     
-  if "data" not in request_data:
+  if "data" not in data:
     return jsonify({
         "status": "fail",
         "code": 400,
         "message": "Data is missing in the request body",
     }), 400
-
-  data = np.array(request_data["data"]).reshape(1, -1)
     
-  prediction = model.predict(data)
+  data_to_array = []
+  for symptom in symptoms:
+    if symptom in data["data"]:
+      data_to_array.append(1)
+    else:
+      data_to_array.append(0)
 
-  predicted_label_index = np.argmax(prediction)
-
+  array_reshape = np.array(data_to_array).reshape(1, -1)
+  
+  prediction = model.predict(array_reshape)
+  
+  predicted_disease = list_diseases[np.argmax(prediction)]
+  
+  print(predicted_disease)
+  
+  get_translated_index = 0
+  
+  for i in range(len(db_diseases)):
+    if db_diseases[i]["disease"] == predicted_disease:
+      get_translated_index = i
+      
+  detail_disease = db_diseases[get_translated_index]
+    
   return jsonify({
     "status": "success",
     "code": 200,
     "message": "Prediction successful",
-    "data": {
-      "prediction": disease[predicted_label_index],
-      "probability": str(int(prediction[0][predicted_label_index] * 100)) + "%"
-    }
+    "prediction": detail_disease
   }), 200
 
-
-  
-
-  
 if __name__ == "__main__":
   app.run(debug=True)
